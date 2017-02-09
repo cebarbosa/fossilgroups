dcdd74da56547030d7c27be97502e2dc656f0667
kadubarbosa <kadu.barbosa@gmail.com>
2017-02-09 08:05:26 -0200

    Fitthing with MPOLY=20.

--- run_ppxf.py
+++ run_ppxf.py
@@ -16,6 +16,7 @@ import pyfits as pf
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
 from astropy.stats import sigma_clip
+from pandas import rolling_std
 
 from ppxf.ppxf import ppxf, reddening_curve
 import ppxf.ppxf_util as util
@@ -120,7 +121,8 @@ class pPXF():
         ax.plot(self.w[self.goodpixels], self.galaxy[self.goodpixels] -
                 self.bestsky[self.goodpixels], "-k", lw=2.,
                 label="Data (S/N={0})".format(np.around(self.sn,1)))
-        ax.plot(self.w[self.goodpixels], self.bestfit[self.goodpixels] - self.bestsky[self.goodpixels], "-", lw=2., c="r",
+        ax.plot(self.w[self.goodpixels], self.bestfit[self.goodpixels] -
+                self.bestsky[self.goodpixels], "-", lw=2., c="r",
                 label="SSPs: V={0:.0f} km/s, $\sigma$={1:.0f} km/s".format(
                     sol[0], sol[1]))
         ax.xaxis.set_ticklabels([])
@@ -133,7 +135,7 @@ class pPXF():
         #     ax.plot(self.w[self.goodpixels], self.bestsky[self.goodpixels], \
         #             "-", lw=1, c="g", label="Sky")
         ax.set_xlim(self.w[0], self.w[-1])
-        # ax.set_ylim(-5 * self.noise, 1.4 * self.bestfit.max())
+        ax.set_ylim(-5 * self.noise, 1.2 * self.galaxy[self.goodpixels[500:-500]].max())
         leg = plt.legend(loc=2, prop={"size":8}, title=self.title,
                          frameon=False)
         # leg.get_frame().set_linewidth(0.0)
@@ -148,7 +150,7 @@ class pPXF():
                  label="$\chi^2=${0:.2f}".format(self.chi2))
         leg2 = plt.legend(loc=2, prop={"size":8})
         ax1.axhline(y=0, ls="--", c="k")
-        # ax1.set_ylim(-8 * self.noise, 8 * self.noise)
+        ax1.set_ylim(-3 * self.noise.mean(), 3 * self.noise)
         ax1.set_xlabel(r"$\lambda$ ($\AA$)", size=12)
         ax1.set_ylabel(r"$\Delta$Flux", size=12)
         gs.update(hspace=0.075, left=0.16, bottom=0.14, top=0.98, right=0.96)
@@ -193,7 +195,8 @@ def snr(flux, axis=0):
     return signal, noise, signal / noise
 
 
-def run_ppxf(group, specs, redo=False, ncomp=2, **kwargs):
+def run_ppxf(group, specs, redo=False, ncomp=2, logdir="ppxf", window = 50,
+             w1=None, w2=None, **kwargs):
     """ New function to run pPXF. """
     global velscale
     tempfile = os.path.join(home, "MILES/templates/"
@@ -215,11 +218,19 @@ def run_ppxf(group, specs, redo=False, ncomp=2, **kwargs):
     ##########################################################################
     for spec in specs:
         os.chdir(os.path.join(data_dir, group))
-        outdir = os.path.join(os.getcwd(), "ppxf")
+        outdir = os.path.join(os.getcwd(), logdir)
         if not os.path.exists(outdir):
             os.mkdir(outdir)
         data = pf.getdata(spec)
         w = wavelength_array(spec)
+        if w1 == None:
+            w1 = w[0]
+        if w2 == None:
+            w2 = w[-1]
+        idx = np.where((w >= w1) & (w <= w2))[0]
+        data = data[idx]
+        w = w[idx]
+        #######################################################################
         # Preparing output
         output = os.path.join(outdir, spec.replace(".fits", ".pkl"))
         outroot = output.replace(".pkl", "")
@@ -227,22 +238,32 @@ def run_ppxf(group, specs, redo=False, ncomp=2, **kwargs):
             continue
         print group, spec
         signal, noise, sn = snr(data)
-        galaxy, logLam, vtemp = util.log_rebin([w[0], w[-1]],             \
+        galaxy, logLam, vtemp = util.log_rebin([w[0], w[-1]],
                                                   data, velscale=velscale)
-        dv = (logLam_temp[0]-logLam[0])*c
         lam = np.exp(logLam)
-        noise =  np.ones_like(galaxy) * noise
-        # kwargs["lam"] = lam
         ###################################################################
+        # Trimming spectra
+        idx = np.where(np.exp(logLam) > np.exp(logLam_temp[0]))[0]
+        didx = int(1500. / velscale) # Space in the beginning of the spec
+        idx = idx[didx:]
+        galaxy = galaxy[idx]
+        lam = lam[idx]
+        logLam = logLam[idx]
+        #######################################################################
         # Masking bad pixels
-        goodpixels = np.arange(len(lam))
+        goodpixels = np.arange(len(lam), dtype=float)
         gaps= set_badpixels(group)
         for gap in gaps:
-            bad = np.argwhere((lam < gap[0]) | (lam > gap[1])).ravel()
-            goodpixels = np.intersect1d(goodpixels, bad)
+            idx = np.where((lam > gap[0] - window/2.) &
+                           (lam < gap[1] + window / 2.))[0]
+            goodpixels[idx] = np.nan
+        goodpixels = goodpixels[~np.isnan(goodpixels)].astype(int)
         kwargs["goodpixels"] = goodpixels
-        ###################################################################
+        #######################################################################
+        kwargs["lam"] = lam
+        dv = (logLam_temp[0] - logLam[0]) * c
         kwargs["vsyst"] = dv
+        noise = np.ones_like(galaxy) * noise
         # First fitting to obtain realistic noise
         pp0 = ppxf(templates, galaxy, noise, velscale, **kwargs)
         pp0.has_emission = True
@@ -256,9 +277,12 @@ def run_ppxf(group, specs, redo=False, ncomp=2, **kwargs):
         pp0.title = ""
         pp0 = pPXF(pp0, velscale)
         pp0.calc_sn()
+        res = (pp0.galaxy - pp0.bestfit)
+        noise = rolling_std(res, window, center=True)
+        noise[:window/2] =  noise[window+1]
+        noise[-window/2+1:] = noise[-window/2]
         # Second fitting using results from first interaction
-        pp = ppxf(templates, galaxy, np.ones_like(galaxy) * pp0.noise,
-                   velscale, **kwargs)
+        pp = ppxf(templates, galaxy, noise, velscale, **kwargs)
         title = "Group {} , Spectrum {}".format(group.upper(), spec.replace(
             ".fits", "").replace("spec", ""))
         # Adding other things to the pp object
@@ -279,15 +303,19 @@ def run_ppxf(group, specs, redo=False, ncomp=2, **kwargs):
     return
 
 
-def make_table(group):
+def make_table(group, logdir="ppxf"):
     """ Make table with results. """
-    head = ("{0:<30}{1:<14}{2:<14}{3:<14}{4:<14}{5:<14}{6:<14}{7:<14}"
+    head4 = "{0:<25}{1:<14}{2:<14}{3:<14}{4:<14}{5:<14}{6:<14}{7:<14}" \
              "{8:<14}{9:<14}{10:<14}{11:<14}{12:<14}\n".format("# FILE",
              "V", "dV", "S", "dS", "h3", "dh3", "h4", "dh4", "chi/DOF",
-             "S/N", "ADEGREE", "MDEGREE"))
+             "S/N", "ADEGREE", "MDEGREE")
+    head2 = "{0:<25}{1:<14}{2:<14}{3:<14}{4:<14}{9:<14}" \
+             "{10:<14}{11:<14}{12:<14}\n".format("# FILE",
+             "V", "dV", "S", "dS", "h3", "dh3", "h4", "dh4", "chi/DOF",
+             "S/N", "ADEGREE", "MDEGREE")
     print "Producing summary for group {0}".format(group)
-    os.chdir(os.path.join(data_dir, group, "ppxf"))
-    output = "ppxf_{}.dat".format(group)
+    os.chdir(os.path.join(data_dir, group, logdir))
+    output = "{}_{}.dat".format(group, logdir)
     results = []
     for i in range(1,20):
         fname = "spec{}".format(i)
@@ -297,7 +325,9 @@ def make_table(group):
         except:
             continue
         sol = pp.sol if pp.ncomp == 1 else pp.sol[0]
+        sol[0] = sol[0]
         error = pp.error if pp.ncomp == 1 else pp.error[0]
+        head = head2 if len(sol) == 2 else head4
         line = np.zeros((sol.size + error.size,))
         line[0::2] = sol
         line[1::2] = error
@@ -310,11 +340,11 @@ def make_table(group):
         f.write(head)
         f.write("".join(results))
 
-def plot_all(group):
+def plot_all(group, specs, logdir="ppxf"):
     """ Make plot of all fits. """
-    os.chdir(os.path.join(data_dir, group, "ppxf"))
-    specs = [x for x in os.listdir(".") if x.endswith(".fits")]
+    os.chdir(os.path.join(data_dir, group, logdir))
     for i, spec in enumerate(specs):
+        print "Print spectrum ", spec
         pp = ppload("{0}".format(spec.replace(".fits", "")))
         pp = pPXF(pp, velscale)
         pp.plot("{0}".format(spec.replace(".fits", ".png")))
@@ -331,25 +361,96 @@ def set_badpixels(group):
     else:
         return None
 
+def test_noise(group, specs, logdir):
+    """ Using previous fittings to improve weighting of fitting. """
+    os.chdir(os.path.join(data_dir, group, logdir))
+    for i in range(1,20):
+        fname = "spec{}".format(i)
+        try:
+            pp = ppload(fname)
+            pp = pPXF(pp, velscale)
+        except:
+            continue
+        res = (pp.galaxy - pp.bestfit) / pp.mpoly
+        from pandas import rolling_std
+        window = 50
+        noise = rolling_std(res, window, center=True)
+        noise[:window/2] =  noise[window+1]
+        noise[-window/2+1:] = noise[-window/2]
+        plt.plot(pp.lam, res, "-k")
+        plt.plot(pp.lam, noise, "-r")
+        plt.plot(pp.lam, -noise, "-r")
+        plt.show(block=1)
+
+
+def plot_zoom(group, specs, logdir):
+    """ Modified version of the plot to include zooms in selected areas of
+    the spectrum. """
+    # Set bands to be plotted
+    bname = os.path.join(home, "tables/bands.txt")
+    features = np.loadtxt(bname, usecols=(0,), dtype="S")
+    bands = np.loadtxt(bname, usecols=(2,3,4,5,6,7))
+    os.chdir(os.path.join(data_dir, group, logdir))
+    fig = plt.figure(1)
+    for i, spec in enumerate(specs):
+        pp = ppload("{0}".format(spec.replace(".fits", "")))
+        pp = pPXF(pp, velscale)
+        fname = os.path.join(data_dir, group, spec)
+        z = pp.sol[0][0] / c
+        print pp.sol[0]
+        data = pf.getdata(fname)
+        w = wavelength_array(fname)
+        lam = pp.lam[pp.goodpixels]
+        apoly = pp.poly[pp.goodpixels]
+        mpoly = pp.mpoly[pp.goodpixels]
+        galaxy = pp.galaxy[pp.goodpixels]
+        bestfit = pp.bestfit[pp.goodpixels]
+        # plt.plot(w, data, "-ok")
+        # plt.plot(lam, bestfit, "-r")
+        # plt.plot(lam, bestfit / mpoly, "-g")
+        # plt.plot(lam, 1000 * mpoly, "-b")
+        # plt.ylim(-300, 3850)
+        for j in range(25):
+            ax = plt.subplot(5,5,j+1)
+            w1 = bands[j,0] * (1 + z)
+            w2 = bands[j,-1] * (1 + z)
+            idx1 = np.where((w >= w1) & (w <= w2))[0]
+            idx2 = np.where((lam >= w1) & (lam <= w2))[0]
+            plt.plot(w[idx1], data[idx1], "o-k", label=features[j])
+            plt.plot(lam[idx2], bestfit[idx2], "-r")
+            plt.legend()
+        plt.show(block=1)
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     groups = ["RXJ0216", "ESO552"]
     vrefs = [19000., 9000.]
+    w1, w2 = 4000, 5400
     for i, (vref, group) in enumerate(zip(vrefs, groups)):
         os.chdir(os.path.join(data_dir, group))
-        start = np.array([[vref, 150., 0., 0.], [vref, 30., 0., 0.]])
-        bounds = np.array([[[vref - 1000., vref + 1000.], [15., 800.],
+        start = np.array([[vref, 50., 0., 0.], [vref, 30., 0., 0.]])
+        bounds = np.array([[[vref -1000., vref + 1000.], [15., 800.],
                             [-0.5, 0.5], [-0.5, 0.5]],
                            [[vref - 1000., vref + 1000.], [15., 200.],
                             [-0.5, 0.5], [-0.5, 0.5]]])
         kwargs = {"start": start,
-                  "plot": False, "moments": [4, 4], "degree": 10,
-                  "mdegree": 4, "reddening": None, "clean": False,
-                  "bounds": bounds}
-
+                  "plot": False, "moments": [4, 2], "degree": -1,
+                  "reddening": None, "clean": False,
+                  "bounds": bounds, "bias" : 0.7, "mdegree": 16}
+        logdir = "ppxf_mom{0}_bias{1}".format(kwargs["moments"][0],
+                                              kwargs["bias"])
         specs = [x for x in os.listdir(".") if x.endswith(".fits")]
-        run_ppxf(group, specs, redo=True, **kwargs)
-        make_table(group)
-        plot_all(group)
-
+        # run_ppxf(group, specs, redo=True, logdir=logdir, w1=w1, w2=w2,
+        #          **kwargs)
+        # make_table(group, logdir=logdir)
+        # plot_all(group, specs, logdir=logdir)
+        # test_noise(group, specs, logdir)
+        plot_zoom(group, specs, logdir)
 
